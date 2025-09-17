a3978166e3e51ce734138aa772a21a42f9ce3916
Lorenzo-Zoccadelli <lorenzo.zoccadelli@studio.unibo.it>
2025-09-17 12:28:03 +0200

    qak sprint 0

--- sprint0/cargorobot_sprint0/Arch_cargorobot.py
+++ sprint0/cargorobot_sprint0/Arch_cargorobot.py
@@ -25,4 +25,16 @@ with Diagram('cargorobotArch', show=False, outformat='png', graph_attr=graphattr
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
 ### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctx_productervice', graph_attr=nodeattr):
+          productservice=Custom('productservice','./qakicons/symActorWithobjSmall.png')
+     with Cluster('ctx_cargoservice', graph_attr=nodeattr):
+          cargoservice=Custom('cargoservice','./qakicons/symActorWithobjSmall.png')
+     with Cluster('ctx_cargorobot', graph_attr=nodeattr):
+          cargorobot=Custom('cargorobot','./qakicons/symActorWithobjSmall.png')
+     sys >> Edge( label='fineCaricamentoContainer', **evattr, decorate='true', fontcolor='darkgreen') >> cargoservice
+     sys >> Edge( label='risoluzioneAnomalia', **evattr, decorate='true', fontcolor='darkgreen') >> cargoservice
+     cargorobot >> Edge( label='fineCaricamentoContainer', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+     sys >> Edge( label='risoluzioneAnomalia', **evattr, decorate='true', fontcolor='darkgreen') >> cargorobot
+     cargoservice >> Edge(color='magenta', style='solid', decorate='true', label='<recuperaProdotto<font color="darkgreen"> dettagliProdotto prodottoNonTrovato</font> &nbsp; >',  fontcolor='magenta') >> productservice
+     cargoservice >> Edge(color='blue', style='solid',  decorate='true', label='<caricamentoContainer &nbsp; >',  fontcolor='blue') >> cargorobot
 diag
