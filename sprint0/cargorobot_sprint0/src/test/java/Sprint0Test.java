a07306fef6704aeec262cc97818a5350d8b72dcd
Lorenzo-Zoccadelli <lorenzo.zoccadelli@studio.unibo.it>
2025-09-18 15:21:47 +0200

    .

---
+++ sprint0/cargorobot_sprint0/src/test/java/Sprint0Test.java
@@ -0,0 +1,23 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import unibo.basicomm23.interfaces.Interaction;
+
+public class Sprint0Test {
+
+	private final static String hostname= "localhost";
+	private final static String portProductService	= "8080";
+	private final static String portCargoService	= "8081";
+
+	private Interaction connProductService;
+    private Interaction connCargoService;
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
