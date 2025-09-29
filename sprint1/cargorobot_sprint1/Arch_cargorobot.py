5a51f7cd4e67d8757c343e4e436f1f9e5c47e5de
zocca02 <lorizocca232@gmail.com>
2025-09-29 10:10:25 +0200

    prima implementazione cargorobot

---
+++ sprint1/cargorobot_sprint1/Arch_cargorobot.py
@@ -0,0 +1,35 @@
+### conda install diagrams
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+evattr = {
+    'color': 'darkgreen',
+    'style': 'dotted'
+}
+with Diagram('cargorobotArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
+     with Cluster('ctx_cargorobot', graph_attr=nodeattr):
+          cargorobot=Custom('cargorobot','./qakicons/symActorWithobjSmall.png')
+     with Cluster('ctx_basicrobot', graph_attr=nodeattr):
+          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
+     cargorobot >> Edge( label='alarm', **eventedgeattr, decorate='true', fontcolor='red') >> sys
+     sys >> Edge( label='risoluzioneAnomalia', **evattr, decorate='true', fontcolor='darkgreen') >> cargorobot
+     cargorobot >> Edge(color='magenta', style='solid', decorate='true', label='<engage<font color="darkgreen"> engagedone engagerefused</font> &nbsp; moverobot<font color="darkgreen"> moverobotdone moverobotfailed</font> &nbsp; >',  fontcolor='magenta') >> basicrobot
+diag
